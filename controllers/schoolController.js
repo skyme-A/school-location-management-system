a8eaad8a790c183299c2d33e6803977fc9671087
Aditi Raj <aditiraj8538@gmail.com>
2026-04-17 00:08:51 +0530

    Clean controller route fix

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -1,37 +1,38 @@
 const db = require("../config/db");
 
-exports.addSchool = (req, res) => {
+const addSchool = (req, res) => {
     const { name, latitude, longitude } = req.body;
 
-    const sql = "INSERT INTO schools (name, latitude, longitude) VALUES (?, ?, ?)";
+    const sql =
+        "INSERT INTO schools (name, latitude, longitude) VALUES (?, ?, ?)";
 
     db.query(sql, [name, latitude, longitude], (err, result) => {
-        if (err) return res.status(500).json(err);
-        res.json({ message: "School added successfully" });
+        if (err) {
+            console.log("DB Error:", err);
+            return res.status(500).json(err);
+        }
+
+        res.json({
+            success: true,
+            message: "School added successfully",
+        });
     });
 };
 
-exports.listSchools = (req, res) => {
-    const userLat = parseFloat(req.query.latitude) || 28.7041;
-    const userLng = parseFloat(req.query.longitude) || 77.1025;
+const listSchools = (req, res) => {
+    const sql = "SELECT * FROM schools";
 
-    db.query("SELECT * FROM schools", (err, result) => {
-        if (err) return res.status(500).json(err);
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log("DB Error:", err);
+            return res.status(500).json(err);
+        }
 
-        const schoolsWithDistance = result.map((school) => {
-            const distance = Math.sqrt(
-                Math.pow(userLat - school.latitude, 2) +
-                Math.pow(userLng - school.longitude, 2)
-            );
-
-            return {
-                ...school,
-                distance,
-            };
-        });
-
-        schoolsWithDistance.sort((a, b) => a.distance - b.distance);
-
-        res.json(schoolsWithDistance);
+        res.json(result);
     });
+};
+
+module.exports = {
+    addSchool,
+    listSchools,
 };
--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,31 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../config/db");
-const { listSchools } = require("../controllers/schoolController");
 
-router.post("/addSchool", async (req, res) => {
-    try {
-        const { name, latitude, longitude } = req.body;
+const {
+    addSchool,
+    listSchools,
+} = require("../controllers/schoolController");
 
-        const sql =
-            "INSERT INTO schools (name, latitude, longitude) VALUES (?, ?, ?)";
-
-        db.query(sql, [name, latitude, longitude], (err, result) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).json(err);
-            }
-
-            res.json({
-                success: true,
-                message: "School added successfully"
-            });
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
-    }
-});
+router.post("/addSchool", addSchool);
 router.get("/listSchools", listSchools);
 
 module.exports = router;
