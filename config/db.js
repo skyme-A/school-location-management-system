6b2edc7bc7483489b3266b130f6b5467f8797c96
Aditi Raj <aditiraj8538@gmail.com>
2026-04-16 23:05:54 +0530

    Fix Aiven SSL connection

--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
-require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-db.connect((err) => {
-    if (err) console.log(err);
-    else console.log("MySQL Connected");
-});
-
-module.exports = db;
+module.exports = connection;
