bf2bed599e7b2d2ef35812899e228b742bfc177e
Aditi Raj <aditiraj8538@gmail.com>
2026-04-16 23:09:14 +0530

    Fix render port

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.get("/health", (req, res) => {
     res.json({ status: "API Running" });
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
