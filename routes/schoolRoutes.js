0c7042480cffc9367e36a50f8106d27d7be82fd7
Aditi Raj <aditiraj8538@gmail.com>
2026-04-16 23:51:30 +0530

    Fix db import

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const { addSchool, listSchools } = require("../controllers/schoolController");
+const db = require("../config/db");
+const { listSchools } = require("../controllers/schoolController");
 
-router.post("/addSchool", addSchool);
+router.post("/addSchool", async (req, res) => {
+    try {
+        const { name, latitude, longitude } = req.body;
+
+        const sql =
+            "INSERT INTO schools (name, latitude, longitude) VALUES (?, ?, ?)";
+
+        db.query(sql, [name, latitude, longitude], (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json(err);
+            }
+
+            res.json({
+                success: true,
+                message: "School added successfully"
+            });
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
 router.get("/listSchools", listSchools);
 
 module.exports = router;
