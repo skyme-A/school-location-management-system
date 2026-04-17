54722c24d720414a12d05733ab90f7c696aad6ed
Aditi Raj <aditiraj8538@gmail.com>
2026-04-17 16:01:31 +0530

    fix db table creation

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./config/db");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+db.query(`
+CREATE TABLE IF NOT EXISTS schools (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  address VARCHAR(255),
+  latitude FLOAT,
+  longitude FLOAT
+)
+`, (err) => {
+  if (err) {
+    console.error("Table creation error:", err);
+  } else {
+    console.log("Schools table ready");
+  }
+});
+
 const schoolRoutes = require("./routes/schoolRoutes");
 app.use("/api", schoolRoutes);
 
 app.get("/health", (req, res) => {
-    res.json({ status: "API Running" });
+  res.json({ status: "API Running" });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
